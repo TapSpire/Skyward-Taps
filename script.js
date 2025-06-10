648199ad5c8958877bb7664f397eee8f692552d3
TapSpire <greensuccess225@gmail.com>
2025-06-09 22:36:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -237,6 +237,13 @@ function checkScoreForFireworks() {
   }
 }
 
+  function playClickSound() 
+  {
+    const sound = document.getElementById('click-sound');
+    sound.currentTime = 0; // Reset to start
+    sound.play();
+  }
+
 // Detect user clicks on the circle and update the score
 circle.addEventListener("click", () => {
   let hoverText = document.createElement("div");
